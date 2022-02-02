fb888b1af83f6cd72eeb6be16e4273b20ae3da29
golangrustnode <hacklinux.org@gmail.com>
2022-02-02 16:11:39 +0800

    add test

---
+++ generatestring_test.go
@@ -0,0 +1,20 @@
+package struct2str
+
+import "testing"
+
+type User struct {
+	Name string
+	Age int
+	Id string
+	Signature string
+	KKK string
+}
+
+func TestGenerateString(t *testing.T) {
+	u := &User{"TangXiaodong", 100, "0000123","FUCK","Fkdas"}
+	str,err := GenerateString(u)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(str)
+}
