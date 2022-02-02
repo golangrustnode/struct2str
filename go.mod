ba00ca580a097a20d46e5abd74cbe5b4d26932b9
golangrustnode <hacklinux.org@gmail.com>
2022-02-02 15:40:56 +0800

    struct 2 string

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/golangrustnode/struct2str
+
+go 1.17
+
+require github.com/luckydog8686/logs v0.0.0-20210131112525-882190a54f4d
+
+require (
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
---
+++ struct2str.go
@@ -0,0 +1,66 @@
+package struct2str
+
+import (
+	"errors"
+	"fmt"
+	"github.com/luckydog8686/logs"
+	"reflect"
+	"sort"
+	"strings"
+)
+
+func GenerateString(obj interface{}) (string, error) {
+	if reflect.TypeOf(obj).Kind() == reflect.Ptr {
+		if reflect.TypeOf(obj).Elem().Kind() == reflect.Struct {
+			return GenerateStringFromStructPtr(obj,true)
+		}
+	}
+	if reflect.TypeOf(obj).Kind() == reflect.Struct {
+		return GenerateStringFromStructPtr(obj,false)
+	}
+	return "", errors.New("Unsupport Type")
+}
+
+func GenerateStringFromStructPtr(obj interface{},isPtr bool) (string, error) {
+	var t reflect.Type
+	var iValue reflect.Value
+	if isPtr{
+		t = reflect.TypeOf(obj).Elem()
+		iValue = reflect.ValueOf(obj).Elem()
+	}else {
+		t = reflect.TypeOf(obj)
+		iValue = reflect.ValueOf(obj)
+	}
+
+
+	numField := t.NumField()
+	structKeys := make([]string, 0, numField)
+	for i := 0; i < numField; i++ {
+		structKeys = append(structKeys, t.Field(i).Name)
+	}
+	sort.Strings(structKeys)
+	msg := ""
+	for i := 0; i < numField; i++ {
+		fieldName := structKeys[i]
+		key := fieldName
+		field, ok := t.FieldByName(fieldName)
+		if !ok {
+			return "", errors.New(fmt.Sprintf("illegal filed name in reflect:%s", fieldName))
+		}
+		jsonTagValue, ok := field.Tag.Lookup("json")
+		if ok {
+			key = jsonTagValue
+		}
+		if strings.ToUpper(key)=="SIGN" || strings.ToUpper(key)=="SIGNATURE"{
+			continue
+		}
+
+		if i == 0 {
+			msg = fmt.Sprintf("%s=%v", key,iValue.FieldByName(fieldName).Interface())
+		}else{
+			msg = fmt.Sprintf("%s&%s=%v",msg, key,iValue.FieldByName(fieldName).Interface())
+		}
+		logs.Info(msg)
+	}
+	return msg, nil
+}
